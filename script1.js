f933553c5295d9c3549112499d5264ebad52c211
Miroslava Herrera <miroslava.herrera@gmail.com>
2019-09-26 08:35:11 -0500

    nuevos

---
+++ script1.js
@@ -0,0 +1,18 @@
+// console.log("hola mundo desde archivo");
+//otro
+//var x=15;
+//x >= 0 ? console.log(x) : console.log(-x);
+//otro
+//{x=Math.PI;
+//cx=Math.cos(x);
+//console.log('cx:', cx)}
+//otro
+//var nombre = "Miroslava";
+//function (saludo){return "hola " + nombre;}
+//otro
+n_nms=2;
+if (n_nms < 1){
+  console.log( "no hay mensajes")
+}else{
+    console.log("hay ", n_nms, "mensajes nuevos")
+}
